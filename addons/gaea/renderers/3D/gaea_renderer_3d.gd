3fce359699281534cfa54ee5049edda865da3727
Ian Cullum <cullumi@wwu.edu>
2025-02-22 00:38:40 -0800

    Added general render_thread class that supports all 2D and 3D gaea renderers.

--- addons/gaea/renderers/3D/gaea_renderer_3d.gd
+++ addons/gaea/renderers/3D/gaea_renderer_3d.gd
@@ -16,17 +16,16 @@ func _draw_area(area: AABB) -> void:
 ## Draws the chunk at [param chunk_position].
 func _draw_chunk(chunk_position: Vector3i) -> void:
 	_draw_area(AABB(chunk_position * generator.chunk_size, generator.chunk_size))
-	chunk_rendered.emit(chunk_position)
+	chunk_rendered.emit.call_deferred(chunk_position)
 
 
 ## Draws the whole grid.
 func _draw() -> void:
 	_draw_area(generator.grid.get_area())
-	grid_rendered.emit()
+	grid_rendered.emit.call_deferred()
 
 
 func _connect_signals() -> void:
-	super()
-
 	if generator.has_signal("chunk_updated"):
 		generator.chunk_updated.connect(_draw_chunk)
+	super()
