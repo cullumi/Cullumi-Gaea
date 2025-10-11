72241ca5c4d660474424a8b0318187822d7aa65a
Ian Cullum <cullumi@wwu.edu>
2025-10-11 03:53:33 -0700

    Simplified GaeaGraph's _duplicate method to only what actually gets saved to it's resource file.

--- addons/gaea/resources/gaea_graph.gd
+++ addons/gaea/resources/gaea_graph.gd
@@ -430,11 +430,12 @@ func _get_unique_resources() -> Dictionary[Variant, GaeaNodeResource]:
 func _duplicate(_subresources:bool = false) -> Resource:
 	var new_graph = GaeaGraph.new()
 	
-	new_graph.resources = _get_unique_resources()
-	new_graph.resource_uids = resource_uids.duplicate(true)
-	new_graph.connections = connections.duplicate(true)
-	new_graph.node_data = node_data.duplicate(true)
-	new_graph.parameters = parameters.duplicate(true)
-	new_graph.other = other.duplicate(true)
+	#new_graph.resources = _get_unique_resources()
+	#new_graph.resource_uids = resource_uids.duplicate(true)
+	new_graph._connections = _connections.duplicate(true)
+	new_graph._node_data = _node_data.duplicate(true)
+	new_graph._parameters = _parameters.duplicate(true)
+	new_graph.save_version = save_version
+	#new_graph._other = other.duplicate(true)
 	
 	return new_graph
